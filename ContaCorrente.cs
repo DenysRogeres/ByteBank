59cd25cf3a394023eba8825ca25e4df1e8e45319
Denys Rógeres <dennysrogeres@gmail.com>
2022-05-17 20:43:05 -0300

    Criando projeto ByteBank e adicionando metodos de saque, deposito e transferencia

---
+++ ContaCorrente.cs
@@ -0,0 +1,41 @@
+﻿namespace ByBank
+{
+    public class ContaCorrente
+    {
+        public string nome;
+        public int numero;
+        public int agencia;
+        public double saldo;
+
+        public bool Sacar(double valor)
+        {
+            if (this.saldo < valor)
+                return false;
+            else
+            {
+                this.saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaCorrente)
+        {
+            if (this.saldo < valor)
+                return false;
+            else
+            {
+                this.saldo -= valor;
+                contaCorrente.saldo += valor;
+
+                return true;
+            }
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ByBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDenys = new ContaCorrente();
+            ContaCorrente contaRayssa = new ContaCorrente();
+
+            contaDenys.nome = "Denys";
+            contaDenys.saldo = 500;
+
+            contaRayssa.nome = "Rayssa";
+            contaRayssa.saldo = 100;
+
+            Console.WriteLine("Conta Denys " + contaDenys.saldo);
+            Console.WriteLine("Conta Rayssa " + contaRayssa.saldo);
+
+            bool resultadoTransferencia = contaDenys.Transferir(2000, contaRayssa);
+
+            Console.WriteLine("Conta Denys " + contaDenys.saldo);
+            Console.WriteLine("Conta Rayssa " + contaRayssa.saldo);
+
+
+            if (resultadoTransferencia == true)
+                Console.WriteLine("Transferência realizada!");
+            else
+                Console.WriteLine("Transferência não realizada!");
+           
+            Console.ReadLine();
+        }
+    }
+}
